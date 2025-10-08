632fe2198298d6c786dceec952190c84ae16d38f
DeathFish38 <juliavuong378@gmail.com>
2025-10-08 12:06:28 +0700

    Adding layout and pages

---
+++ Frontend/Service/IngredientService.cs
@@ -0,0 +1,26 @@
+public class IngredientService
+{
+    private List<Ingredient> _ingredients = new();
+    public void AddIngredient(Ingredient ingredient)
+    {
+        _ingredients.Add(ingredient);
+    }
+
+    public void DeleteIngredient(Ingredient ingredient)
+    {
+        _ingredients.Remove(ingredient);
+    }
+
+    //add edit ingredients
+
+}
+
+public class Ingredient
+{
+    public int Id { get; set;  }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public int Quantity { get; set; }
+    public DateTime OpenedDate { get; set; }
+    public DateTime ExpiredDate { get; set; }
+}
---
+++ Frontend/Service/ShoppinglistService.cs
@@ -0,0 +1,25 @@
+public class ShoppinglistService()
+{
+
+    private readonly List<Shoppinglist> _list = new Shoppinglist();
+    public IEnumerable<ShoppingListModel> GetAll() => _lists;
+
+    public void Add(Shoppinglist list)
+    {
+        if (list.Title is not null)
+        {
+            _list.Add(list);
+        }
+
+    }
+
+}
+
+public class Shoppinglist
+{
+    public string Title { get; set; } = "";
+    public List<string> Items { get; set; } = new();
+    public DateTime DateCreated { get; set; } = DateTime.Now;
+
+
+}
