544cae7109aac0b09faefac9f6aaf817521d9dff
StarStriker <77044214+luckyseo@users.noreply.github.com>
2025-10-18 19:01:37 +1100

    recipe FE

--- Frontend/Models/Recipe.cs
+++ Frontend/Models/Recipe.cs
@@ -1,14 +1,28 @@
 
 namespace Frontend.Models
 {
-    public class Recipe {
+  public class Recipe
+  {
 
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public List<Ingredient> Ingredients { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string ImageUrl { get; set; }
+    public string Description { get; set; }
+    public List<Ingredient> Ingredients { get; set; } = new();
 
-      public string Category { get; set; }
+    public RecipeCategory Category { get; set; }
 
-    }
+  }
+
+  public enum RecipeCategory
+  {
+        Salad = 0,
+        Soup = 1,
+        Main =2,
+        Side =3,
+        Vegetarian =4,
+        Dessert =5
+  }
+    
+ 
 }
---
+++ Frontend/Service/RecipeService.cs
@@ -0,0 +1,44 @@
+using System.Diagnostics.Contracts;
+using Frontend.Models;
+
+namespace Frontend.Service
+{
+    public class RecipeService
+    {
+        //Call API to get data from backend 
+
+        private readonly HttpClient _httpClient;
+        private readonly LoginService _loginService;
+
+        public RecipeService(HttpClient httpClient, LoginService loginService)
+        {
+            _httpClient = httpClient;
+            _loginService = loginService;
+
+        }
+        //get all recipes
+        public async Task<List<Recipe>> GetAllRecipes()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Recipe>>("http://localhost:5037/api/recipe");
+        }
+
+        //get suggested recipes based on available ingredients
+        public async Task<List<Recipe>> GetSuggestedRecipes(int userId)
+        {
+            return await _httpClient.GetFromJsonAsync<List<Recipe>>($"http://localhost:5037/api/recipe/user/{userId}/suggestion");
+        }
+
+        public async Task<Recipe> GetRecipeById(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<Recipe>($"http://localhost:5037/api/recipe/{id}");
+        }
+
+        public async Task<List<Recipe>>GetRecipeByCategory(string category)
+        {
+            return await _httpClient.GetFromJsonAsync<List<Recipe>>($"http://localhost:5037/api/recipe/category/{category}");
+        }
+ 
+   
+
+    }
+}
